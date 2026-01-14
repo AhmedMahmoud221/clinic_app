633b41a82d7cc17918c8e27c92aba91b10f5f409
AhmedMahmoud221 <a.mahmoud2212000@gmail.com>
2026-01-14 15:31:00 +0200

    create logincubits & loginState & dio factory & app Regex & dependency Injection done login logic

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,8 +1,11 @@
+import 'package:clinic_app/core/di/dependency_injection.dart';
 import 'package:clinic_app/core/routing/routes.dart';
+import 'package:clinic_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:clinic_app/features/login/ui/login_screen.dart';
 import 'package:clinic_app/features/onboarding/onboarding_screen.dart';
 // import 'package:clinic_app/features/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings setting) {
@@ -15,7 +18,12 @@ class AppRouter {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
