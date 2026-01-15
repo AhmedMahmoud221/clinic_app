c17499ccc04906fcdcbc3614cf742999778b3d0a
AhmedMahmoud221 <a.mahmoud2212000@gmail.com>
2026-01-15 12:11:08 +0200

    adding firebase_options and create android & ios versions in firebase to link apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:clinic_app/core/di/dependency_injection.dart';
 import 'package:clinic_app/core/routing/app_router.dart';
 import 'package:clinic_app/doc_app.dart';
+import 'package:clinic_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // to fix texts being hidden bug in flutter_screenutil in release mode
   await ScreenUtil.ensureScreenSize();
   runApp(DocApp(appRouter: AppRouter()));
